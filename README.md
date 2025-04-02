38f3947d30fe84555055162db84a2153f38a1768
Jorney Lopez <jorneycr@hotmail.com>
2025-04-02 08:36:33 -0600

    add more funcionabily to product list

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# TiendaOnline
+# Tienda Online
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 19.1.4.
 
--- src/app/listado-productos/listado-productos.component.ts
+++ src/app/listado-productos/listado-productos.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
 import { ProductoComponent } from "../producto/producto.component";
+import { FormsModule } from '@angular/forms';
+import { Producto } from '../producto/producto.model';
 
 @Component({
   selector: 'app-listado-productos',
-  imports: [ProductoComponent],
+  imports: [FormsModule],
   templateUrl: './listado-productos.component.html',
   styleUrl: './listado-productos.component.css'
 })
 export class ListadoProductosComponent {
 
+  productos: Producto [] = [
+    new Producto('Pantalon', 130.0),
+    new Producto('Camisa', 80.0),
+    new Producto('Tennis', 30.0)
+  ];
+
+  descripcionInput: string = '';
+  precionInput: number | null = null;
+
+  agregarProducto(){
+    //Validar que sean valores correcto
+    if(this.descripcionInput.trim() === '' || this.precionInput == null || this.precionInput <=0){
+      console.log('Debe ingresar una descripción y un precio válidos');
+      return;
+    }
+
+    const producto = new Producto(this.descripcionInput, this.precionInput);
+    this.productos.push(producto);
+
+    // Limpiamos los campos del formulario
+    this.descripcionInput = '';
+    this.precionInput = null;
+  }
 }
