63373a8ba2edfa86e5fd7d4af11585214f3bcbae
jorneycr <jorneycr@hotmail.com>
2025-04-10 11:30:05 -0600

    error page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { ListadoProductosComponent } from './listado-productos/listado-productos.component';
 import { FormularioComponent } from './formulario/formulario.component';
+import { ErrorComponent } from './error/error.component';
 
 export const routes: Routes = [
     {path:'', component: ListadoProductosComponent}, //localhost:4200/
     {path:'listado', component: ListadoProductosComponent},
     {path:'agregar', component: FormularioComponent},
     {path:'editar/:id', component: FormularioComponent},
+    {path:'**', component: ErrorComponent} //localhost:4200/404,
 ];
